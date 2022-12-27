5e9ac876ff004d6f8158abff638e7e4f02ae4c95
Rzvone <bobonea.razvan.ctin@gmail.com>
2022-12-27 12:36:27 +0200

    added brocode.js to index.html for test

---
+++ rockpaperscissors.js
@@ -0,0 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
+console.log(computerSelection);
+
+
+function playGame(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        roundWinner = "Tie";
+    }
+
+}
